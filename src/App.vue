c5ce5208b2eae391361f3826536df12e36adface
Alfred Wallace <7476515+AlfredWallace@users.noreply.github.com>
2020-04-20 01:04:17 +0200

    More funny names + adapting summaries

--- src/App.vue
+++ src/App.vue
@@ -33,34 +33,23 @@
         <div class="p-2 border border-solid shadow-md">
           <p class="my-2">
             Le Grand Tournoi est un format inédit du traditionnel quizz de
-            l'After Foot (animé par Julien Cazarre) sous forme de tableau de
-            duels à élimination directe, commençant en 1/8e de finales. Il est
-            instauré en 2020 suite au confinement national français dû à
-            l'épidémie de Covid-19.
+            l'After Foot (animé par Julien Cazarre) sous forme de tableau à
+            élimination directe, commençant en 1/8e de finales. Il est instauré
+            en 2020 suite au confinement national français dû à l'épidémie de
+            Covid-19.
           </p>
-          <div class="mb-2">
-            Les deux aspects du format du Grand Tournoi sont justifiés ainsi :
-            <ul class="list-disc pl-6">
-              <li>
-                La composition des participants (soit par équipes, soit un grand
-                chacun-pour-soi) est abandonnée au profit de duels, limitant
-                ainsi les chances de propagation du virus.
-              </li>
-              <li>
-                L'organisation (précédemment sous la forme de journées, typique
-                d'un championnat) est abandonnée au profit d'un tableau à
-                élimination directe. Cette mesure est adoptée pour raccourcir la
-                saison et la terminer à tout prix, les pertes des droits TV et
-                des contrats de sponsoring pouvant mettre en danger la santé
-                financière des participants.
-              </li>
-            </ul>
-          </div>
+          <p class="mb-2">
+            L'adoption de ce format à élimination est justifiée par le besoin de
+            raccourcir la saison et la terminer à tout prix, les pertes des
+            droits TV et des contrats de sponsoring pouvant mettre en danger la
+            santé financière des participants.
+          </p>
           <p>
-            Le tournoi verra s'affronter les 8 participants habituels classés
-            selon leur rang à la Race au moment du début du Grand Tournoi, ainsi
-            que 8 invités plus ou moins prestigieux, assignés de manière
-            aléatoire. Chaque match se joue en 10 minutes.
+            Le tournoi verra s'affronter 8 équipes habituelles du quizz,
+            classées selon leur position dans le championnat au moment du début
+            du Grand Tournoi, ainsi que 8 équipes invitées plus ou moins
+            prestigieuses, assignées de manière aléatoire. Chaque match se joue
+            en 10 minutes.
           </p>
         </div>
       </PageSectionComponent>
@@ -84,7 +73,7 @@ export default {
   data() {
     return {
       teams: {
-        gbr: new Team("Racing Club Brisbois", "Racing", 1),
+        gbr: new Team("RC Strasbrisbois", "Racing", 1),
         max: new Team("Royal Standard de Max", "Standard", null),
         edm: new Team("Olympique Di Meco", "OM", null),
         kdi: new Team("Ajax Diazterdam", "Ajax", 8),
@@ -99,7 +88,7 @@ export default {
         nja: new Team("Nicolas", "Jamain", 7),
         lch: new Team("Inter Charbonara FC", "Inter", null),
         pve: new Team("Paul", "Vexiau", null),
-        dri: new Team("Daniel", "Riolo", 2)
+        dri: new Team("AS Rioloma", "Rioloma", 2)
       }
     };
   },
@@ -112,13 +101,13 @@ export default {
           new Date(2020, 3, 7),
           new Side(this.teams.gbr, 6),
           new Side(this.teams.max, 5),
-          "A priori l'une des affiches les plus déséquilibrée, le match démarre par un 2-0 du favori Gilbert Brisbois. Max revient à 2-1, mais la technique et l'experience de Gilbert vont porter le score à 4-1." +
-            " Une certaine fébrilité s'empare de ce dernier lorsque Max marque son 2e but, et, sentant que le vent commence à tourner, il profitera non pas d'une mais de 2 erreurs de Gilbert pour revenir à 4 partout." +
-            " Pris de panique, Gilbert répond à une question dans la règle mais pas dans l'esprit, profitant d'une faille du règlement en énumérant rapidement des réponses dans l'espoir de tomber juste par hasard," +
+          "A priori l'une des affiches les plus déséquilibrée, le match démarre par un 2-0 du favori, le RC Strasbrisbois. Le Standard revient à 2-1, mais la technique et l'experience des strasbrisbois vont porter le score à 4-1." +
+            " Une certaine fébrilité s'empare de ces derniers lorsque l'outsider marque son 2e but, et, sentant que le vent commence à tourner, ils profiteront non pas d'une mais de 2 erreurs du Racing pour revenir à 4 partout." +
+            " Pris de panique, le RC Strasbrisbois répond à une question dans la règle mais pas dans l'esprit, profitant d'une faille du règlement en énumérant rapidement des réponses dans l'espoir de tomber juste par hasard," +
             " suscitant les huées du public. L'arbitre accorde le but mais prévient la tête de série que ce comportement ne sera plus toléré (appelée plus tard la jurisprudence Hénin-Beaumont)." +
-            " Gilbert retrouve un peu son jeu et passe la marque à 6-4, mais la rage de l'injustice donne la force à Max de réduire à nouveau l'écart. Sentant la fin du temps règlemtaire arriver," +
-            " Gilbert met une énorme pression sur l'arbitre et perd du temps, ce qui fonctionnera puisque ce dernier arrêtera le match à 6-5 alors qu'une dernière action aurait pu être jouée." +
-            " Max, dont on n'attendait rien, sort dès le premier tour mais par la grande porte, vainqueur moral de ce match d'ouverture. Gilbert, déjà accusé régulièrement d'être favorisé par le corps arbitral, ne redorera pas son image."
+            " Retrouvant un peu son jeu, il passe la marque à 6-4, mais la rage de l'injustice donne la force au Standard de réduire à nouveau l'écart. Sentant la fin du temps règlemtaire arriver," +
+            " les strasbrisbois mettent une énorme pression sur l'arbitre et perdent du temps, ce qui fonctionnera puisque ce dernier arrêtera le match à 6-5 alors qu'une dernière action aurait pu être jouée." +
+            " Le Standard, dont on n'attendait rien, sort dès le premier tour mais comme un roi, vainqueur moral de ce match d'ouverture. Le Racing, déjà accusé régulièrement d'être favorisé par le corps arbitral, ne redorera pas son image."
         ),
         new Match(
           16,
@@ -126,7 +115,14 @@ export default {
           new Date(2020, 3, 9),
           new Side(this.teams.edm, 4),
           new Side(this.teams.kdi, 7),
-          "Première grosse affiche de ces huitièmes de finale. D'un côté, Kèvin Diaz, huitième de la Race mais considéré par beaucoup d'observateurs comme ayant un niveau supérieur à son classement. De l'autre, Éric Di Meco, invité, mais premier d'une autre championnat, considéré par beaucoup d'observateurs comme une potentielle tête de série 1 bis. Mais, la vie nous réserve parfois des surprises, comme disait Brandao c'est comme la boîte à chocolat. En effet, le scénario de ce huitième touche du doigt l'irrationnalité pure. Kévin ouvre la marque, et Éric enchaîne sur un but contre son camp, 2-0. Certains disent que le visage de ce dernier au coup d'envoi n'annoncait rien de bon. On ne le saura jamais, mais ce dont on est sûr c'est que c'est ce moment là qu'il choisit pour perdre son football. Cauchemar devenu réalité, les minutes qui suivirent verront Kévin porter la marque à 7-0, dont un second CSC. Le monde entier est en état de stupeur, le match est plié. Mais, tout va très vite dans le football, et le haut niveau se joue souvent dans les têtes. À peine son premier but marqué pour revenir à 7-1, Éric se transforma en monstre d'efficacité et de précision et enchaîna 3 autres buts, 7-4 au tableau d'affichage. Malheureusement le réveil fut trop tardif, et c'est bien Kévin qui se qualifia, plutôt soulagé que le temps réglementaire arrive à son terme."
+          "Première grosse affiche de ces huitièmes de finale. D'un côté, L'Ajax Diazterdam, jeune équipe huitième de la Race mais très technique, et considérée par beaucoup d'observateurs comme ayant un niveau supérieur à son classement." +
+            " De l'autre, L'Olympique Di Meco, invitée, mais premieère d'un autre championnat, considérée par ces mêmes observateurs comme une potentielle tête de série 1 bis." +
+            " La vie nous réserve parfois des surprises, ainsi que disait Brandao : c'est comme la boîte à chocolat. En effet, le scénario de ce huitième touche du doigt l'irrationnalité pure." +
+            " L'Ajax ouvre la marque, et l'OM enchaîne par un but contre son camp, 2-0. Certains disent que les visages des olympiens au coup d'envoi n'annoncaient rien de bon. On ne le saura jamais," +
+            " mais ce dont on est sûr c'est que c'est ce moment là qu'ils choisirent pour perdre leur football. Cauchemar devenu réalité, les minutes qui suivirent verront la fougue de la jeunesse porter la marque à 7-0," +
+            " dont un second CSC. Le monde entier est en état de stupeur, le match est plié. Mais, tout va très vite dans le football, et le haut niveau se joue souvent dans les têtes. À peine son premier but marqué pour revenir à 7-1," +
+            " l'OM se transforma en monstre d'efficacité et de précision et enchaîna 3 autres buts. 7-4 au tableau d'affichage ! Malheureusement le réveil fut trop tardif, et c'est bien l'Ajax qui se qualifia," +
+            " plutôt soulagée que le temps réglementaire arriva à son terme."
         ),
         new Match(
           16,
@@ -142,7 +138,9 @@ export default {
           new Date(2020, 3, 8),
           new Side(this.teams.pdu, 4),
           new Side(this.teams.jmh, 8),
-          "Ce match fut marqué par l'une des pires premières périodes des huitièmes de finale, les 2 protagonistes ratant pratiquement toutes leurs occasions. Lors de la seconde, le niveau de jeu redevint acceptable, le favori Jonathan assumant son statut et dominant logiquement la rencontre. Un peu amer, Pierre eu quelques mots en fin de match, accusant son adversaire de tricherie, mais l'on vit bien qu'il n'en était pas lui-même convaincu. Victoire logique 8-4 de la tête de série numéro 4."
+          "Ce match fut marqué par l'une des pires premières périodes des huitièmes de finale, les 2 protagonistes ratant pratiquement toutes leurs occasions, à tel point qu'il n'y a quasiment rien à en raconter." +
+            " Lors de la seconde, le niveau de jeu redevint acceptable, le favori Godot F.C. assumant son statut et dominant logiquement la rencontre, sans jamais vraiment l'emballer." +
+            " Un peu amer, Pierre eu quelques mots en fin de match, accusant son adversaire de tricherie, mais l'on vit bien qu'il n'en était pas lui-même convaincu. Victoire logique 8-4 de la tête de série numéro 4."
         ),
         new Match(
           16,
@@ -150,7 +148,9 @@ export default {
           new Date(2020, 3, 16),
           new Side(this.teams.jro, 11),
           new Side(this.teams.aro, 4),
-          "Ce huitième est marqué par le forfait de Nicolas Vilas, remplacé au pied levé par Arthur Robert. Ce forfait est un coup dur pour les organisateurs du Grand Tournoi, l'affiche faisant normalement s'affronter l'un des invités les plus forts avec la tête de série numéro 3. Le match fut décevant, la différence de niveau entre les protagonistes étant trop élevée. Qualification facile pour la patte gauche 11-4."
+          "Ce huitième est marqué par le forfait de Nicolas Vilas, remplacé au pied levé par Arthur Robert. Ce forfait est un coup dur pour les organisateurs du Grand Tournoi," +
+            " l'affiche faisant normalement s'affronter l'un des invités les plus forts avec la tête de série numéro 3. Le match fut décevant, la différence de niveau entre les protagonistes étant trop élevée." +
+            " Qualification facile pour la patte gauche 11-4."
         ),
         new Match(
           16,
